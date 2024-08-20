9d99eedbcaa5d5609b63e37db9d31a8b5b612de9
Dmitry Tokmak <dimka.listok@gmail.com>
2024-08-20 11:31:54 +0300

    fid duuble hit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/setupTests'],
+    mockReset: true,
+  },
   css: {
     preprocessorOptions: {
       scss: {
